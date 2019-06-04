1edd42b5d76f9613f9e0559baeae78ef33940e02
charles161 <fortloop.charles@gmail.com>
2019-06-04 17:27:02 +0530

    docker files added

--- server.js
+++ server.js
@@ -1,34 +1,49 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
-
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const morgan = require("morgan");
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl:true
-  }
+    client: "pg",
+    connection: {
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_DB,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD
+    }
 });
 
 const app = express();
 
-app.use(cors())
+app.use(morgan("combined"));
+app.use(cors());
 app.use(bodyParser.json());
+app.get("/cool", (req, res) => {
+    res.json("hi there");
+});
+app.get("/", (req, res) => {
+    res.send(db.users);
+});
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", (req, res) => {
+    register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+    profile.handleProfileGet(req, res, db);
+});
+app.put("/image", (req, res) => {
+    image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+    image.handleApiCall(req, res);
+});
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
-
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT || 3000}`);
-})
+app.listen(3000, () => {
+    console.log(`app is running on port ${3000}`);
+});
